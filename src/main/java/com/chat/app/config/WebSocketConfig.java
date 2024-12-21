000b96dbe89ce78eea38776f3aa3f2b1de8e1dcd
Hrushiingole <hrishikeshingole532@gmail.com>
2024-12-21 11:03:10 +0530

    added chatMessage model and mappings in controller

--- src/main/java/com/chat/app/config/WebSocketConfig.java
+++ src/main/java/com/chat/app/config/WebSocketConfig.java
@@ -20,6 +20,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chats").setAllowedOrigins("http://localhost:5173").withSockJS();
+        registry.addEndpoint("/chats").setAllowedOrigins("http://localhost:8080").withSockJS();
     }
 }
